063d0dceacf3db09a7e4a4fb663876389d1cbb9b
sirdan <danielsdfullerton@gmail.com>
2021-05-25 14:00:34 +0100

    fixed and tidied api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -9,44 +9,97 @@ function headers() {
   }
 }
 
-// requesting wines
+//* Post Requests
+
+// .posts
 
 export function getAllPosts() {
-  return axios.get(`${baseUrl}/posts/all`)
-}
-
-export function getSingleUser(userId) {
-  return axios.get(`${baseUrl}/profile/${userId}`)
+  return axios.get(`${baseUrl}/posts`)
 }
 
 export function createPost(formdata) {
   return axios.post(`${baseUrl}/posts`, formdata, headers())
 }
 
-export function createComment(postId, formdata) {
-  return axios.post(`${baseUrl}/posts/${postId}`, formdata, headers())
-}
+// .posts.postId
 
-export function createFollower(userId, formdata) {
-  return axios.post(`${baseUrl}/profile/${userId}`, formdata, headers())
-}
-
-export function deletePost(postId) {
-  return axios.delete(`${baseUrl}/posts/${postId}`, headers())
+export function getSinglePost(postId) {
+  return axios.get(`${baseUrl}/posts/${postId}`)
 }
 
 export function editPost(postId, formdata) {
   return axios.put(`${baseUrl}/posts/${postId}`, formdata, headers())
 }
 
+export function likePost(postId) {
+  return axios.post(`${baseUrl}/posts/${postId}`, headers())
+}
+
+export function deletePost(postId) {
+  return axios.delete(`${baseUrl}/posts/${postId}`, headers())
+}
 
 
-// auth requests
+
+//* Comment Requests
+
+// .posts.postId.comments
+
+export function createComment(postId, formdata) {
+  return axios.post(`${baseUrl}/posts/${postId}/comments`, formdata, headers())
+}
+
+// .posts.postId.comments.commentId
+
+export function editComment(postId, commentId, formdata) {
+  return axios.put(`${baseUrl}/posts/${postId}/comments/${commentId}`, formdata, headers())
+}
+
+export function likeComment(postId, commentId) {
+  return axios.post(`${baseUrl}/posts/${postId}/comments/${commentId}`, headers())
+}
+
+export function deleteComment(postId, commentId) {
+  return axios.delete(`${baseUrl}/posts/${postId}/comments/${commentId}`, headers())
+}
+
+//* User Profile Requests
+
+// .profile
+
+export function getAllUsers() {
+  return axios.get(`${baseUrl}/profile`)
+}
+
+// .profile.profileId
+
+export function getSingleUser(profileId) {
+  return axios.get(`${baseUrl}/profile/${profileId}`)
+}
+
+export function getSingleUser(profileId, formdata) {
+  return axios.put(`${baseUrl}/profile/${profileId}`, formdata, headers())
+}
+
+//* Auth Requests
+
+// .login
 
 export function loginUser(formdata) {
   return axios.post(`${baseUrl}/login`, formdata)
 }
 
+// .register
+
 export function registerUser(formdata) {
   return axios.post(`${baseUrl}/register`, formdata)
 }
+
+
+
+
+// ! Stretch goals
+
+// export function createFollower(userId, formdata) {
+//   return axios.post(`${baseUrl}/profile/${userId}`, formdata, headers())
+// }
