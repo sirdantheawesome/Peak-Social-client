ae0d2825404394eeb21d12f0adec36c1bf8dd199
bradb345 <bradbernard17@gmail.com>
2021-05-25 16:25:36 +0100

    postIndex

--- src/components/posts/PostIndex.js
+++ src/components/posts/PostIndex.js
@@ -1,4 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { getAllPosts } from '../../lib/api'
+import Error from '../common/Error'
+
 function PostIndex() {
+  const [posts, setPosts] = React.useState(null)
+  const [isError, setIsError] = React.useState(false)
+  const isLoading = !posts && !isError
+
+  React.useEffect(()=>{
+    const getData = async () => {
+      try {
+        const res = await getAllPosts()
+        setPosts(res.data)
+      } catch (error) {
+        setIsError(true)
+      }
+    }
+    getData()
+  }, [])
   return (
     <h1>All posts for non peek users to view</h1>
   )
