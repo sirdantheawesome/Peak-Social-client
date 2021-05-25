c365adab7328c76b9715369d762d240a95d4ff13
sirdan <danielsdfullerton@gmail.com>
2021-05-25 11:58:47 +0100

    First pass Nav bar

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import PostNew from './components/posts/PostNew'
 import PostIndex from './components/posts/PostIndex'
 import UserProfile from './components/user/UserProfile'
 import SecureRoute from './components/common/SecureRoute'
+import Nav from './components/common/Nav'
 
 
 function App() {
   return (
 
     <Router>
+      <Nav />
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/profile" component={UserProfile}/>
-        <SecureRoute path="/posts/new" component={PostNew}/>
-        <Route path="/posts/all" component={PostIndex}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/profile/:userId" component={UserProfile} />
+        <SecureRoute path="/posts/new" component={PostNew} />
+        <Route path="/posts/all" component={PostIndex} />
       </Switch>
     </Router>
-  
+
   )
 }
 
